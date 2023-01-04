cb1649eb82b40b0968f6c0a62916063f14c56a2b
Alireza.k <ahmidyly@gmail.com>
2023-01-04 14:21:48 +0330

    change

--- first_p.py
+++ first_p.py
@@ -3,17 +3,20 @@ address = {}
 
 def addNewName_Phone1(name,phone):
     tel.update({name: phone})
+    print('Your name saved!.')
 
 def addAdress2(name,loc):
     x = tel.keys()
     if name in x:
         address.update({name:loc})
+        print('Your address saved!.')
     else:
         print('not found')
 
 def delWithName3(name):
     tel.pop(name)
     address.pop(name)
+    print('Your name and was deleted!.')
 
 def search_phone_number4(name):
     if name in tel:
@@ -23,6 +26,7 @@ def change_name5(name, newname):
     if name in (tel and address):
         tel[newname] = tel.pop(name)
         address[newname] = address.pop(name)
+        print('Your name changed.')
     else:
         print('not found!')
 
@@ -39,42 +43,56 @@ def sorting6():
 
 
 
-test = {'momad' : '1231' ,'wwer' : 'ewr', 'hd' : '12' ,'ali':'021'}
+'''test = {'momad' : '1231' ,'wwer' : 'ewr', 'hd' : '12' ,'ali':'021'}
 d = {'ali' : 'asdas'}
 
 print('before : ', test)
 test.pop('momad')
-print(test)
+print(test)'''
 
+while True:
+    print('1. Add name and phone number.')
+    print('2. Add name and address.')
+    print('3. Delete phone number and address.')
+    print('4. Searching for phone number.')
+    print('5. Change your name.')
+    print('6. Print sorting value of tel and address dict.')
 
-print('1. Add name and phone number.')
-print('2. Add name and address.')
-print('3. Delete phone number and address.')
-print('4. Searching for phone number.')
-print('5. Change your name.')
-print('6. Print sorting value of tel and address dict.')
+    print()
+    n = int(input('Please enter a number : '))
 
-n = int(input('Please enter a number : '))
+    if n >= 1 and n <=6:
+        if n==1:
+            na = input('Your name : ')
+            phone_n = input('Your phone number : ')
 
-if n >= 1 and n <=6:
-    if n==1:
-        na = input('Your name : ')
-        phone_n = input('Your phone number : ')
+            addNewName_Phone1(na, phone_n)
+            print()
+        elif n==2:
+            na = input('Your name : ')
+            loc = input('Your address : ')
 
-        addNewName_Phone1(na, phone_n)
-    elif n==2:
-        na = input('Your name : ')
-        loc = input('Your address : ')
+            addAdress2(na, loc)
+            print()
+        elif n==3:
+            na = input('Your name : ')
 
-        addAdress2(na, loc)
-    elif n==3:
-        na = input('Your name : ')
+            delWithName3(na)
+            print()
+        elif n==4:
+            na = input('Your name : ')
+            search_phone_number4(na)
+            print()
+        elif n==5:
+            na = input('Your name : ')
+            na2 = input('Enter your new name : ')
 
-        delWithName3(na)
-    elif n==4:
-    elif n==5:
-    elif n==6:
-    elif n==7:
-        pass
+            change_name5(na, na2)
+            print()
+        elif n==6:
+            sorting6()
+            print()
+        elif n==7:
+            break
 
 
