f90ea648e27fca49f33190f9d5201f0fa706bf0a
Alireza.k <ahmidyly@gmail.com>
2023-01-17 15:26:19 +0330

    get matrix

--- matrix_det.py
+++ matrix_det.py
@@ -1,9 +1,5 @@
-n = int(input('Enter a size of matrix : '))
-b = [[None] * n] *n
-print('This is b : ', b)
+import os
 
-for i in range(n):
-    for j in range(n):
-        b[i][j] = int(input())
+q = [[int(input('Enter number : ')) for i in range(2) ] for j in range(2)]
 
-print(b)
+
